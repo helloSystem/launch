8cbc22e567e55cad70a76db70340515624272d3b
probonopd <probonopd@users.noreply.github.com>
2020-10-17 14:44:17 +0200

    More error handlers

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 #include <QFileInfo>
 #include <QStandardPaths>
 #include <QDirIterator>
-#include <QCommandLineParser>
+#include <QTime>
+#include <QElapsedTimer>
+#include <QRegExpValidator>
 
 /*
 
@@ -15,7 +17,7 @@ https://github.com/probonopd/appwrapper
 and:
 GNUstep openapp
 
-TODO: Make the behavior resemble:
+TODO: Make the behavior resemble /usr/local/GNUstep/System/Tools/openapp (a bash script)
 
 user@FreeBSD$ /usr/local/GNUstep/System/Tools/openapp --help
 usage: openapp [--find] [--debug] application [arguments...]
@@ -43,6 +45,62 @@ public:
     }
 };
 
+// If a package needs to be updated, tell the user how to do this,
+// or even offer to do it
+QString getPackageUpdateCommand(QString pathToInstalledFile){
+    QString candidate = QStandardPaths::findExecutable("pkg");
+    if (candidate != "") {
+        // We are on FreeBSD
+        QProcess p;
+        QString exe = "pkg which " + pathToInstalledFile;
+        p.start(exe);
+        p.waitForFinished();
+        QString output(p.readAllStandardOutput());
+        if (output != "") {
+            qDebug() << output;
+            QRegExp rx(".* was installed by package (.*)-(.*)\n");
+            if (rx.indexIn(output) == 0) {
+                return QString("sudo pkg install %1").arg(rx.cap(1));
+            }
+        }
+    }
+    // TODO: Implement the same for deb and rpm...
+    // In all other cases, return a blank string
+    return "";
+}
+
+// Translate cryptic errors into clear text, and possibly even offer buttons to take action
+void handleError(QDetachableProcess *p, QString errorString){
+    QRegExp rx(".*ld-elf.so.1: (.*): version (.*) required by (.*) not found.*");
+    QRegExp rxPy(".*ModuleNotFoundError: No module named '(.*)'.*");
+    if(errorString.contains("FATAL: kernel too old")) {
+        QString cleartextString = "The Linux compatibility layer reports an older kernel version than what is required to run this application.\n\n" \
+                                  "Please run\nsudo sysctl compat.linux.osrelease=5.0.0\nand try again.";
+        QMessageBox::warning( nullptr, p->program(), cleartextString );
+    } else if (rx.indexIn(errorString) == 0) {
+        QString outdatedLib = rx.cap(1);
+        QString versionNeeded = rx.cap(2);
+        QFile f(outdatedLib);
+        QFileInfo fileInfo(f.fileName());
+        QString outdatedLibShort(fileInfo.fileName());
+        QString requiredBy = rx.cap(3);
+        QString cleartextString = QString("This application requires at least version %2 of %1 to run.").arg(outdatedLibShort).arg(versionNeeded);
+        if (getPackageUpdateCommand(outdatedLib) != "") {
+            cleartextString.append(QString("\n\nPlease update it with\n%1\nand try again.").arg(getPackageUpdateCommand(outdatedLib)));
+        } else {
+            cleartextString.append(QString("\n\nPlease update it and try again.").arg(getPackageUpdateCommand(outdatedLib)));
+        }
+        QMessageBox::warning( nullptr, p->program(), cleartextString );
+    } else if (rxPy.indexIn(errorString) == 0) {
+        QString missingPyModule = rxPy.cap(1);
+        QString cleartextString = QString("This application requires the Python module %1 to run.\n\nPlease install it and try again.").arg(missingPyModule);
+        QMessageBox::warning( nullptr, p->program(), cleartextString );
+    } else {
+        QMessageBox::warning( nullptr, p->program(), errorString );
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
 
@@ -51,20 +109,12 @@ int main(int argc, char *argv[])
 
     QApplication app(argc, argv);
 
-    QCommandLineParser parser;
-    parser.setApplicationDescription("Launch an application and show a graphical error dialog in case the launch did not succeed");
-    parser.addHelpOption();
-
-    parser.addPositionalArgument("application", QCoreApplication::translate("main", "Application to be launched. Can be an absolute or relative path to an executable, the name of an executable on the $PATH, or the name of an .AppDir"));
-
-    parser.process(app);
-
     QStringList args = app.arguments();
 
     args.pop_front();
 
     if(args.isEmpty()){
-        qDebug() << "No arguments were supplied";
+        qDebug() << "USAGE:" << argv[0] << "<application to be launched> [<arguments>]" ;
         exit(1);
     }
 
@@ -101,12 +151,33 @@ int main(int argc, char *argv[])
     // the contents of many desktop files, plist files, etc. here. If we were using a database,
     // this restriction could be removed.
     if(executable == nullptr) {
-        QStringList locationsContainingApps = { "/Applications", "/System", "/Library",
+
+        // Measure the time it takes to look up candidates
+        QElapsedTimer timer;
+        timer.start();
+
+        QStringList locationsContainingApps = {};
+
+        // Add some location in $HOME
+        locationsContainingApps.append(QDir::homePath() + "/Applications");
+        locationsContainingApps.append(QDir::homePath() + "/bin");
+        locationsContainingApps.append(QDir::homePath() + "/.bin");
+
+        // Add system-wide locations
+        // TODO: Find a better and more complete way to specify the GNUstep ones
+        locationsContainingApps.append({"/Applications", "/System", "/Library",
                                                "/usr/local/GNUstep/Local/Applications",
-                                               "/usr/local/GNUstep/System/Applications" // TODO: Find a better and more complete way to specify these
-                                              };
-        // TODO: Also search for executables in the other types of supported files, e.g.,
-        // QStandardPaths::ApplicationsLocation = "~/.local/share/applications", "/usr/local/share/applications", "/usr/share/applications"
+                                               "/usr/local/GNUstep/System/Applications",
+                                               "/usr/GNUstep/Local/Applications",
+                                               "/usr/GNUstep/System/Applications"
+                                              });
+
+        // Add legacy locations for XDG compatibility
+        // On FreeBSD: "/home/user/.local/share/applications", "/usr/local/share/applications", "/usr/share/applications"
+        locationsContainingApps.append(QStandardPaths::standardLocations(QStandardPaths::ApplicationsLocation));
+
+        qDebug() << locationsContainingApps;
+
         // Iterate recursively through locationsContainingApps searching for AppRun files in matchingly named AppDirs
         QFileInfoList candidates;
         foreach (QString directory, locationsContainingApps) {
@@ -140,6 +211,9 @@ int main(int argc, char *argv[])
                 }
             }
         }
+
+        qDebug() << "Took" << timer.elapsed() << "milliseconds to find candidates via the filesystem";
+
         foreach (QFileInfo candidate, candidates) {
             // Now that we may have collected different candidates, decide on which one to use
             // e.g., the one with the highest self-declared version number. Again, a database might come in handy here
@@ -172,18 +246,26 @@ int main(int argc, char *argv[])
     // Errors occuring thereafter will not be reported to the user in a message box anymore.
     // This should cover most errors like missing libraries, missing interpreters, etc.
 
+
+
     if (p.state() == 0 and p.exitCode() != 0) {
         qDebug("Process is not running anymore and exit code was not 0");
 
         const QString error = p.readAllStandardError();
         if (!error.isEmpty()) {
-            QMessageBox::warning( nullptr, p.program(), error );
+            qDebug() << error;
+            handleError(&p, error);
             return(p.exitCode());
         }
     } else {
+        // Print normal output to stdout; test with e.g., 'launch ls'
+        QString out = p.readAllStandardOutput();
+        QTextStream(stdout) << out;
+
         p.detach();
         return(0);
     }
 
     return(1);
 }
+
